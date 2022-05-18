1dd162537e780d8876bdff29776213f70862fb04
taiyangAh <wulian404@gmail.com>
2022-05-18 12:22:27 +0800

    set up web project connecting to the deployed contract on Rinkeby

--- pages/[id].js
+++ pages/[id].js
@@ -1,31 +1,35 @@
-import { useRouter } from 'next/router'
-import Head from 'next/head'
+import { useRouter } from "next/router";
+import Head from "next/head";
 
-import { web3, contract, contractAddress } from '../lib/web3'
+import { web3, contract, contractAddress } from "../lib/web3";
 
-import Navigation from '../components/Navigation'
-import Universe from '../components/Universe'
-import EthName from '../components/EthName'
+import Navigation from "../components/Navigation";
+import Universe from "../components/Universe";
+import EthName from "../components/EthName";
 
-import metadata from '../planetary-data/metadata-3.json'
+import metadata from "../planetary-data/metadata-1.json";
 
 function Planet({ metadata }) {
-  const router = useRouter()
-  let { id } = router.query
-  id = parseInt(id)
+  const router = useRouter();
+  let { id } = router.query;
+  id = parseInt(id);
 
   if (typeof document === "object") {
-    document.documentElement.style.setProperty("--main-color", metadata.properties.mainColor)
-    document.documentElement.style.setProperty("--secondary-color", metadata.properties.secondaryColor)
+    document.documentElement.style.setProperty(
+      "--main-color",
+      metadata.properties.mainColor
+    );
+    document.documentElement.style.setProperty(
+      "--secondary-color",
+      metadata.properties.secondaryColor
+    );
   }
 
   // TODO
-  let forSaleNotice = (
-    <p>Not for sale</p>
-  )
+  let forSaleNotice = <p>Not for sale</p>;
 
   // TODO
-  let openSeaLink = "#"
+  let openSeaLink = "#";
 
   return (
     <>
@@ -37,7 +41,7 @@ function Planet({ metadata }) {
 
         {forSaleNotice}
 
-        <a href={openSeaLink} target="_blank" className="button"> 
+        <a href={openSeaLink} target="_blank" className="button">
           View on OpenSea
         </a>
       </section>
@@ -46,29 +50,28 @@ function Planet({ metadata }) {
         <title>Planetary – Planet #{id}</title>
       </Head>
     </>
-  )
+  );
 }
 
 export async function getStaticPaths() {
-  const paths = [1, 2, 3, 4, 5].map(id => {
-    return { params: { id: id.toString() }}
-  })
+  const paths = [1, 2, 3, 4, 5].map((id) => {
+    return { params: { id: id.toString() } };
+  });
 
   return {
     paths: paths,
-    fallback: true
-  } 
+    fallback: true,
+  };
 }
 
 export async function getStaticProps({ params }) {
-
   // params.id is available
 
   return {
     props: {
-      metadata: metadata
-    }
-  }
+      metadata: metadata,
+    },
+  };
 }
 
-export default Planet
+export default Planet;
